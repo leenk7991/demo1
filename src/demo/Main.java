c8ca9417f5c25a153e596831eb5ef3f98737ce6d
Leen Kilani <leen.kilani@gmail.com>
2020-04-20 22:02:56 +0300

    Update Main.java

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world from local pc");
+	System.out.println("hello world from github");
 	// write your code here
     }
 }
