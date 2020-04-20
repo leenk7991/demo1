360e3efa1d038e98ef3f908871c81770796d5741
Leen Kilani <leen.kilani@gmail.com>
2020-04-20 22:04:42 +0300

    branch1 first commit

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world from local pc");
-	System.out.println("hello world from github");
+	    System.out.println("hello world from github");
+	    System.out.println("this is branch1");
 	// write your code here
     }
 }
