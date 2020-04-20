692ace29f21f67bf3ea2cdaa3306fdc7618e94e8
Leen Kilani <leen.kilani@gmail.com>
2020-04-20 22:06:04 +0300

    Update Main.java

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("hello world from local pc");
 	    System.out.println("hello world from github");
 	    System.out.println("this is branch1");
+	    System.out.println("edit after branch1");
 	// write your code here
     }
 }
